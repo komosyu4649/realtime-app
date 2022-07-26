d47a887909597e3e41c8420979fd181d73abc314
komosyu4649 <komosyu4649@gmail.com>
2022-07-27 00:18:37 +0900

    add: userProfile

--- components/DashBoard.tsx
+++ components/DashBoard.tsx
@@ -1,17 +1,31 @@
-import { FC } from 'react'
+import { FC, Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 import { LogoutIcon, ExclamationCircleIcon } from '@heroicons/react/solid'
 import { supabase } from '../utils/supabase'
+import { Spinner } from './Spinner'
+import { UserProfile } from './UserProfile'
 
 export const DashBoard: FC = () => {
   const signOut = () => {
     supabase.auth.signOut()
   }
   return (
-    <div className="">
+    <>
       <LogoutIcon
         className="my-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
-      ></LogoutIcon>
-    </div>
+      />
+      <div className="item-center flex flex-col">
+        <ErrorBoundary
+          fallback={
+            <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+          }
+        >
+          <Suspense fallback={<Spinner />}>
+            <UserProfile />
+          </Suspense>
+        </ErrorBoundary>
+      </div>
+    </>
   )
 }
