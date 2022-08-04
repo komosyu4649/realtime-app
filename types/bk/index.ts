0954ad27176f3ecfe59b29663bd5b37bf3342ddf
komosyu4649 <komosyu4649@gmail.com>
2022-08-04 09:26:17 +0900

    update: type

--- types/bk/index.ts
+++ types/bk/index.ts
@@ -5,22 +5,26 @@ export type Post = {
   title: string
   post_url: string
 }
+
 export type EditedPost = {
   id: string
   title: string
   post_url: string
 }
+
 export type Comment = {
   id: string
   created_at: string
-  user_id: string | undefined
+  user_id: string
   post_id: string
   comment: string
 }
+
 export type EditedComment = {
   id: string
   comment: string
 }
+
 export type Profile = {
   id: string | undefined
   updated_at: string
@@ -29,17 +33,20 @@ export type Profile = {
   avatar_url: string | undefined
   favorites: string | undefined
 }
+
 export type EditedProfile = {
   username: string | undefined
   avatar_url: string | undefined
   favorites: string | undefined
 }
+
 export type Notice = {
   id: string
   created_at: string
   user_id: string | undefined
   content: string
 }
+
 export type EditedNotice = {
   id: string
   content: string
